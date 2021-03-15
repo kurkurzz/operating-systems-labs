216adb72efdf945d1e117e80d6c7f52fa5ffee73
Muhammad Nurhafiz <64152220+kurkurzz@users.noreply.github.com>
2021-03-15 12:14:50 +0800

    rename lab  1 var (follow python naming convention)

--- Lab 1/best_fit.py
+++ Lab 1/best_fit.py
@@ -1,5 +1,5 @@
-blockSize = [100, 500, 200, 300, 600]
-processSize = [212, 417, 112, 426]
+block_size = [100, 500, 200, 300, 600]
+process_size = [212, 417, 112, 426]
 
 '''
 BEST FIT 
@@ -8,33 +8,33 @@ Implementation:
 1- Input memory blocks and processes with sizes.
 2- Initialize all memory blocks as free.
 3- Start by picking each process and find the
-   minimum block size that can be assigned to
-   current process i.e., find min(bockSize[1], 
-   blockSize[2],.....blockSize[n]) > 
-   processSize[current], if found then assign 
-   it to the current process.
+    minimum block size that can be assigned to
+    current process i.e., find min(bockSize[1], 
+    blockSize[2],.....blockSize[n]) > 
+    processSize[current], if found then assign 
+    it to the current process.
 5- If not then leave that process and keep checking
-   the further processes.
-   '''
+    the further processes.
+    '''
 
-def findMin(size):
-   blockPlaceHolder = 1000
-   locationPlaceHolder = -1
-   for i in range(len(blockSize)):
-      if (blockSize[i] < size): 
-         continue
-      if (blockSize[i] - size < blockPlaceHolder):
-          blockPlaceHolder = blockSize[i] - size
-          locationPlaceHolder = i
-   
-   blockSize[locationPlaceHolder] = blockSize[locationPlaceHolder] - size
-   return(locationPlaceHolder)
+def find_min(size):
+    block_place_holder = 1000
+    location_place_holder = -1
+    for i in range(len(block_size)):
+        if (block_size[i] < size): 
+            continue
+        if (block_size[i] - size < block_place_holder):
+            block_place_holder = block_size[i] - size
+            location_place_holder = i
+    
+    block_size[location_place_holder] = block_size[location_place_holder] - size
+    return(location_place_holder)
 
-def bestFit():
-   print("Process No.\tProcess Size\tBlock no.")
-   for n in range(len(processSize)):
-      blockNumber = findMin(processSize[n])
-      print ("%d\t\t%d\t\t%d" % (n+1, processSize[n], blockNumber+1))
+def best_fit():
+    print("Process No.\tProcess Size\tBlock no.")
+    for n in range(len(process_size)):
+        block_number = find_min(process_size[n])
+        print ("%d\t\t%d\t\t%d" % (n+1, process_size[n], block_number+1))
 
 print("Best Fit:")
-bestFit()
+best_fit()
--- Lab 1/first_fit.py
+++ Lab 1/first_fit.py
@@ -1,5 +1,5 @@
-blockSize = [100, 500, 200, 300, 600]
-processSize = [212, 417, 112, 426]
+block_size = [100, 500, 200, 300, 600]
+process_size = [212, 417, 112, 426]
 
 '''
 FIRST FIT
@@ -8,29 +8,29 @@ Implementation:
 1- Input memory blocks with size and processes with size.
 2- Initialize all memory blocks as free.
 3- Start by picking each process and check if it can
-   be assigned to current block. 
-4- If size-of-process <= size-of-block if yes then 
-   assign and check for next process.
-5- If not then keep checking the further blocks.
-'''
+    be assigned to current block. 
+    4- If size-of-process <= size-of-block if yes then 
+    assign and check for next process.
+    5- If not then keep checking the further blocks.
+    '''
 
-def findFirstFit(size):
-   for x in range(len(blockSize)):
-      if (blockSize[x] > size): 
-         blockSize[x] -= size
-         return x
-   # return value -1 if processSize can't be allocated
-   return -1
+def find_first_fit(size):
+    for x in range(len(block_size)):
+        if (block_size[x] > size): 
+            block_size[x] -= size
+            return x
+    # return value -1 if processSize can't be allocated
+    return -1
 
-def firstFit():
-   print("Process No.\tProcess Size\tBlock no.")
-   for a in range(len(processSize)):
-      blockNumber = findFirstFit(processSize[a])
-      if blockNumber != -1:
-         print ("%d\t\t%d\t\t%d" % (a+1, processSize[a], blockNumber+1))
-      else:
-         print ("%d\t\t%d\t\t%s" % (a+1, processSize[a], "Not Allocated"))
+def first_fit():
+    print("Process No.\tProcess Size\tBlock no.")
+    for a in range(len(process_size)):
+        block_number = find_first_fit(process_size[a])
+        if block_number != -1:
+            print ("%d\t\t%d\t\t%d" % (a+1, process_size[a], block_number+1))
+        else:
+            print ("%d\t\t%d\t\t%s" % (a+1, process_size[a], "Not Allocated"))
 
 
 print("First Fit:")
-firstFit()
+first_fit()
