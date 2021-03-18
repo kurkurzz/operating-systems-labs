911480fcfeb86d9436bb19fd220f8c4ccc1fb472
Ezlan Zulfiqree <66157842+ezzylan@users.noreply.github.com>
2021-03-19 00:24:33 +0800

    first commit

---
+++ Lab 1/best_fit.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+int blockSize[] = {100, 500, 200, 300, 600};
+int processSize[] = {212, 417, 112, 426};
+
+// Implementation
+// 1 - Input memory blocks and processes with sizes.
+// 2 - Initialize all memory blocks as free.
+// 3 - Start by picking each process and find the minimum block size that can be assigned to current process
+//     i.e., find min(blockSize[1], blockSize[2], ..., blockSize[n]) > processSize[current],
+//     if found then assign it to the current process.
+// 4 - If not then leave that process and keep checking the further processes.
+
+int findMin(int processSize);
+void bestFit();
+
+int main()
+{
+    printf("Best Fit:\n");
+    bestFit();
+
+    return 0;
+}
+
+int findMin(int processSize)
+{
+    int allocator = -1;
+    // int blockSizeLength = (int) sizeof(blockSize) / sizeof(blockSize[0]);
+    int blockSizeLength = 5;
+
+    for (int i = 0; i < blockSizeLength; i++)
+    {
+        if (processSize < blockSize[i])
+        {
+            blockSize[i] -= processSize;
+            allocator = i;
+        }
+    }
+
+    return allocator;
+}
+
+void bestFit()
+{
+    // int processSizeLength = (int) sizeof(processSize) / sizeof(processSize[0]);
+    int processSizeLength = 4;
+    printf("Process No.\tProcess Size\tBlock No.\n");
+
+    for (int i = 0; i < processSizeLength; i++)
+    {
+        int blockNumber = findMin(processSize[i]);
+        printf("%d\t\t\t%d\t\t\t%d\n", i + 1, processSize[i], blockNumber);
+        // kena cek balik blockNumber !!!
+    }
+}
---
+++ Lab 1/first_fit.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+int blockSize[] = {100, 500, 200, 300, 600};
+int processSize[] = {212, 417, 112, 426};
+
+// Implementation
+// 1 - Input memory blocks and processes with sizes.
+// 2 - Initialize all memory blocks as free.
+// 3 - Start by picking each process and check if it can be assigned to current block.
+// 4 - If size-of-process <= size-of-block if yes then assign and check for next process.
+// 5 - If not then keep checking the further blocks.
+
+int findFirstFit(int processSize);
+void firstFit();
+
+int main()
+{
+    printf("First Fit:\n");
+    firstFit();
+
+    return 0;
+}
+
+int findFirstFit(int processSize)
+{
+    int blockSizeLength = 5;
+
+    for (int i = 0; i < blockSizeLength; i++)
+    {
+        if (processSize < blockSize[i])
+        {
+            blockSize[i] -= processSize;
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+void firstFit()
+{
+    int processSizeLength = 4;
+    printf("Process No.\tProcess Size\tBlock No.\n");
+
+    for (int i = 0; i < processSizeLength; i++)
+    {
+        int blockNumber = findFirstFit(processSize[i]);
+        if (blockNumber != -1)
+        {
+            printf("%d\t\t\t%d\t\t\t%d\n", i + 1, processSize[i], blockNumber + 1);
+        }
+        else
+        {
+            printf("%d\t\t\t%d\t\t\t%s\n", i + 1, processSize[i], "Not Allocated");
+        }
+    }
+}
